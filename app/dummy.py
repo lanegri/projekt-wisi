1760aec9f9019bb2e5c63d31d83a5c2d3528e9c1
Aminou Memoko <ammem@outlook.de>
2024-07-14 18:11:54 +0200

    first commit

---
+++ app/dummy.py
@@ -0,0 +1,10 @@
+def func_number_greater_100(number: int) -> bool:
+    return number > 100
+
+
+def func_number_greater_equal_100(number: int) -> bool:
+    return number >= 100
+
+
+def func_number_less_200(number: int) -> bool:
+    return number < 200
